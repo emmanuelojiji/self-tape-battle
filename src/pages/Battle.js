750f099721c7addf96e9e7d4b364f333b3ffdb44
emmanuelojiji <emmanuelojiji@hotmail.com>
2023-07-22 22:35:00 +0100

    Admin page added
    battle info retrieved, upload functionality, retrieve video cards

--- src/pages/Battle.js
+++ src/pages/Battle.js
@@ -3,21 +3,85 @@ import { useParams } from "react-router-dom";
 import "./Battle.scss";
 import { battlesObject } from "../battlesObject";
 import Button from "../components/Button";
+import UploadModal from "../components/UploadModal";
+import {
+  collection,
+  getDoc,
+  doc,
+  connectFirestoreEmulator,
+  getDocs,
+  getCountFromServer,
+  setDoc,
+  Timestamp,
+} from "firebase/firestore";
+import { auth, db } from "../firebaseConfig";
+import VideoCard from "../components/VideoCard";
+import VideoModal from "../components/VideoModal";
 
-const Battle = () => {
+const Battle = ({ uploadModalVisible, setUploadModalVisible }) => {
   const { id } = useParams();
 
-  const selectedBattle = battlesObject.find((battle) => battle.id == id);
+  const user = auth.currentUser;
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [battle, setBattle] = useState("");
+
+  const [entries, setEntries] = useState([]);
+
+  const [selectedVideo, setSelectedVideo] = useState();
+
+  const [votes, setVotes] = useState();
+
+  useEffect(() => {
+    const getBattle = async () => {
+      try {
+        const battlesCollectionRef = collection(db, "battles");
+        const battleDoc = await getDoc(doc(battlesCollectionRef, id));
+
+        setBattle(battleDoc.data());
+
+        const entriesCollectionRef = collection(db, "battles", id, "entries");
+
+        const entriesDocs = await getDocs(entriesCollectionRef);
+
+        const entry = entriesDocs.docs.map((doc) => doc.data());
+
+        console.log(entry.length);
+
+        setEntries(entry);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBattle();
+  }, []);
+
 
   return (
     <div>
+      {modalVisible && <VideoModal selectedVideo={selectedVideo} />}
       <div className="battle-header">
-        <div>
-          <h1>{selectedBattle.title}</h1>
-          <h3>{selectedBattle.description}</h3>
-        </div>
+        <div>{/*<h1>{battle.name}</h1>*/}</div>
 
-        <Button text="Upload tape" />
+        <Button
+          text="Upload tape"
+          onClick={() => {
+            setUploadModalVisible(true);
+          }}
+        />
+      </div>
+
+      <div className="entry-card-container">
+        {entries.map((entry) => (
+          <VideoCard
+            onClick={() => {
+              setSelectedVideo(entry.uid);
+              setModalVisible(true)
+              console.log(selectedVideo);
+            }}
+          />
+        ))}
       </div>
     </div>
   );
