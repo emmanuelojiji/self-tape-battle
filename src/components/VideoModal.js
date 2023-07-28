0ae53ea5b9355d584208ccc36a1dad08d6618db3
emmanuelojiji <emmanuelojiji@hotmail.com>
2023-07-28 19:26:25 +0100

    Video modal changes for logged in users' video

--- src/components/VideoModal.js
+++ src/components/VideoModal.js
@@ -42,6 +42,8 @@ const VideoModal = ({ voteCount, selectedVideo, setModalVisible }) => {
       }
     };
 
+
+    //Not necessary anymore as I'm using "You" instead
     const getUser = async () => {
       const docSnapshot = doc(db, "battles", id, "entries", selectedVideo);
 
@@ -99,7 +101,7 @@ const VideoModal = ({ voteCount, selectedVideo, setModalVisible }) => {
         <div className="modal-header">
           <div className="left">
             <Avatar size="35" />
-            <h3>{name}</h3>
+            <h3>You</h3>
           </div>
           <div className="right">
             <div className="num-of-votes">{votes}</div>
