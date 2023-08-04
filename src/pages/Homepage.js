2cf6a8af30c2d153486511f47a8b460c67261e5e
emmanuelojiji <emmanuelojiji@hotmail.com>
2023-08-04 21:27:06 +0100

    Banner copy changed test homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,11 +7,14 @@ const Homepage = () => {
     <div className="homepage">
       <div className="homepage-banner">
         Smile! 🎥 You're in the test version of Self Tape Battle. If you have a
-        referral code, you can begin testing out our features before the app is launched.
+        referral code, you can begin testing out our features before the app is
+        launched. It's still rusty right now, bear with us!
       </div>
       <img src={logo} className="logo" />
 
-      <p>Please switch to a desktop, we're currently building the mobile Arena.</p>
+      <p>
+        Please switch to a desktop, we're currently building the mobile Arena.
+      </p>
       <Link to="/signup">
         <button>Let's go</button>
       </Link>
