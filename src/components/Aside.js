1aaa1c42c51e513466cd8735f7f4752f760a4e33
emmanuelojiji <emmanuelojiji@hotmail.com>
2023-08-08 00:18:47 +0100

    Sliding Nav implemented

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -16,31 +16,49 @@ const Aside = () => {
 
   const { user, storedUserId } = useAuth();
 
+  const [slidePosition, setSlidePosition] = useState(0);
+
   return (
     <aside>
       <img src={Logo} className="logo" />
       <nav>
+        <div
+          className="nav-slider"
+          style={{ transform: `translateY(${slidePosition}px)` }}
+        ></div>
         <Link
           to="/home/battles"
           className={currentPage === "arena" ? "active" : null}
-          onClick={() => setCurrentPage("arena")}
+          onClick={() => {
+            setCurrentPage("arena");
+            setSlidePosition(0);
+          }}
         >
           Arena
         </Link>
         <Link
           to={`/home/profile/${storedUserId}`}
           className={currentPage === "profile" ? "active" : null}
-          onClick={() => setCurrentPage("profile")}
+          onClick={() => {
+            setCurrentPage("profile");
+            setSlidePosition(60);
+          }}
         >
           Profile
         </Link>
 
-        <Link to="/home/directory" className={currentPage === "directory" ? "active" : null} onClick={() => setCurrentPage("directory")}>
+        <Link
+          to="/home/directory"
+          className={currentPage === "directory" ? "active" : null}
+          onClick={() => {
+            setCurrentPage("directory");
+            setSlidePosition(60 * 2);
+          }}
+        >
           Directory
         </Link>
         <Link className="disabled">Wallet</Link>
         <Link className="disabled">Leaderboard</Link>
-
         <Link className="disabled">Shop</Link>
         <Link className="disabled">Green Room</Link>
         <Link className="disabled">Casting Calls</Link>
